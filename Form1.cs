7647a86f392a3812de06958bd4bfab86797d94b2
VanTien2009 <vantien.aten@gmail.com>
2017-09-26 14:48:27 +0700

    Chance

--- Form1.cs
+++ Form1.cs
@@ -11,9 +11,19 @@ namespace Demo
 {
     public partial class Form1 : Form
     {
+        int a;
+        int b;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToInt32( textBox1.Text);
+            b = Convert.ToInt32( textBox2.Text);
+            int kq = a + b;
+            textBox3.Text = kq.ToString();
         }
     }
 }
